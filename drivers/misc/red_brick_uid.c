b7da95e1274f55db1c55c27031b9a59a701fc1d5
Matthias Bolte <matthias@tinkerforge.com>
2014-06-02 12:46:36 +0200

    red_brick_uid: Prepare UID value in module init
    Avoids race conditions in the getters and the need for locking.

--- drivers/misc/red_brick_uid.c
+++ drivers/misc/red_brick_uid.c
@@ -20,8 +20,20 @@
 #define PROC_ENTRY_NAME "red_brick_uid"
 #define BASE58_MAX_STR_SIZE 8
 
-static const char alphabet[] =
+static const char base58_alphabet[] =
 	"123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+static u32 uid;
+static char uid_str[BASE58_MAX_STR_SIZE];
+
+u32 red_brick_get_uid(void)
+{
+	return uid;
+}
+
+const char *red_brick_get_uid_str(void)
+{
+	return uid_str;
+}
 
 static void base58_encode(char *str, u32 value)
 {
@@ -32,12 +44,12 @@ static void base58_encode(char *str, u32 value)
 
 	while (value >= 58) {
 		mod = value % 58;
-		reverse_str[i] = alphabet[mod];
+		reverse_str[i] = base58_alphabet[mod];
 		value = value / 58;
 		++i;
 	}
 
-	reverse_str[i] = alphabet[value];
+	reverse_str[i] = base58_alphabet[value];
 
 	for (k = 0; k <= i; k++) {
 		str[k] = reverse_str[i - k];
@@ -48,69 +60,58 @@ static void base58_encode(char *str, u32 value)
 	}
 }
 
-static u32 uid = 0;
-
-u32 red_brick_get_uid(void)
-{
-	if (uid == 0) {
-		struct sw_chip_id chip_id;
-
-		sw_get_chip_id(&chip_id);
-
-		uid = ((chip_id.sid_rkey0 & 0x000000ff) << 24) | (chip_id.sid_rkey3 & 0x00ffffff);
-
-		// avoid collisions with other Brick UIDs by clearing the 31th bit, as
-		// other Brick UIDs should have the 31th bit set always. avoid collisions
-		// with Bricklet UIDs by setting the 30th bit to get a high UID, as
-		// Bricklets have a low UID
-		uid = (uid & ~(1 << 31)) | (1 << 30);
-	}
-
-	return uid;
-}
-EXPORT_SYMBOL(red_brick_get_uid);
-
-static char uid_str[BASE58_MAX_STR_SIZE] = "";
-
-const char *red_brick_get_uid_str(void)
-{
-	if (uid_str[0] == '\0') {
-		base58_encode(uid_str, red_brick_get_uid());
-	}
-
-	return uid_str;
-}
-EXPORT_SYMBOL(red_brick_get_uid_str);
-
-static int proc_read(char *buffer, char **buffer_location,
-                     off_t offset, int buffer_length, int *eof, void *data)
+static int proc_uid_read(char *buffer, char **buffer_location,
+                         off_t offset, int buffer_length, int *eof, void *data)
 {
 	if (offset > 0) {
 		return 0;
 	}
 
-	return snprintf(buffer, buffer_length, "%s\n", red_brick_get_uid_str());
+	return snprintf(buffer, buffer_length, "%s\n", uid_str);
 }
 
-static int __init init(void)
+static int __init setup(void)
 {
-	struct proc_dir_entry *proc_uid = create_proc_entry(PROC_ENTRY_NAME, S_IRUGO, NULL);
+	struct sw_chip_id chip_id;
+	struct proc_dir_entry *proc_uid;
+
+	/* read chip ID and deduce UID from it */
+	sw_get_chip_id(&chip_id);
+
+	uid = ((chip_id.sid_rkey0 & 0x000000ff) << 24) |
+	       (chip_id.sid_rkey3 & 0x00ffffff);
+
+	/* avoid collisions with other Brick UIDs by clearing the 31th bit,
+	 * as other Brick UIDs should have the 31th bit set always. avoid
+	 * collisions with Bricklet UIDs by setting the 30th bit to get a
+	 * high UID, as Bricklets have a low UID */
+	uid = (uid & ~(1 << 31)) | (1 << 30);
+
+	/* encode the UID as base58 */
+	base58_encode(uid_str, uid);
+
+	/* register proc entry /proc/red_brick_uid */
+	proc_uid = create_proc_entry(PROC_ENTRY_NAME, S_IRUGO, NULL);
 
 	if (!proc_uid) {
 		return -ENOMEM;
 	}
 
-	proc_uid->read_proc = proc_read;
+	proc_uid->read_proc = proc_uid_read;
 
 	return 0;
 }
-module_init(init);
 
 static void __exit cleanup(void)
 {
 	remove_proc_entry(PROC_ENTRY_NAME, NULL);
 }
+
+MODULE_AUTHOR("Matthias Bolte");
+MODULE_LICENSE("GPL");
+
+module_init(setup);
 module_exit(cleanup);
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Matthias Bolte");
+EXPORT_SYMBOL(red_brick_get_uid);
+EXPORT_SYMBOL(red_brick_get_uid_str);
