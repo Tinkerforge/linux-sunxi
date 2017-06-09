5115d1a4e97dab35a7bdd3d22850c49ebf562aaa
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-06-09 12:26:02 +0200

    red-brick: Fix indentation

--- drivers/tty/serial/8250/8250.c
+++ drivers/tty/serial/8250/8250.c
@@ -1592,7 +1592,7 @@ int serial8250_handle_irq(struct uart_port *port, unsigned int iir)
 	unsigned long flags;
 	struct uart_8250_port *up =
 		container_of(port, struct uart_8250_port, port);
-    struct circ_buf *xmit = &port->state->xmit;
+	struct circ_buf *xmit = &port->state->xmit;
 
 	if (iir & UART_IIR_NO_INT)
 		return 0;
@@ -1607,34 +1607,35 @@ int serial8250_handle_irq(struct uart_port *port, unsigned int iir)
 		status = serial8250_rx_chars(up, status);
 
 	serial8250_modem_status(up);
+
 	if (status & UART_LSR_THRE) {
-        int empty = uart_circ_empty(xmit);
+		int empty = uart_circ_empty(xmit);
 
-        if (!empty) {
-            // Assert RTS
-            assert_rts_uart(port);
-            assert_rts_uart8250(up);
-        }
+		if (!empty) {
+			// Assert RTS
+			assert_rts_uart(port);
+			assert_rts_uart8250(up);
+		}
 
-        /*
-         * The following function calls __stop_tx(up) which disables
-         * THRI (THRE in datasheet) interrupt once all bytes are written to the FIFO.
-         * So after the function call we are re-enabling this interrupt.
-         * We need to re-enable this interrupt because we want to deassert the
-         * RTS line once the FIFO and the TX shift register are empty.
-         */
-        serial8250_tx_chars(up);
+		/*
+		 * The following function calls __stop_tx(up) which disables
+		 * THRI (THRE in datasheet) interrupt once all bytes are written to the FIFO.
+		 * So after the function call we are re-enabling this interrupt.
+		 * We need to re-enable this interrupt because we want to deassert the
+		 * RTS line once the FIFO and the TX shift register are empty.
+		 */
+		serial8250_tx_chars(up);
 
-        up->ier |= UART_IER_THRI;
-        serial_out(up, UART_IER, up->ier);
+		up->ier |= UART_IER_THRI;
+		serial_out(up, UART_IER, up->ier);
 
-        if (empty && (status & UART_LSR_TEMT)) {
-            // Deassert RTS
-            deassert_rts_uart(port);
-            deassert_rts_uart8250(up);
-            __stop_tx(up);
-        }
-    }
+		if (empty && (status & UART_LSR_TEMT)) {
+			// Deassert RTS
+			deassert_rts_uart(port);
+			deassert_rts_uart8250(up);
+			__stop_tx(up);
+		}
+	}
 
 	spin_unlock_irqrestore(&port->lock, flags);
 	return 1;
